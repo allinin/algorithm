f8f44721ebf96ce79c0d5c27cfdd74f3efbaa7a7
ZBL <813321674@qq.com>
2019-08-27 11:40:52 +0800

    2019.08.20

--- src/org/sd/Adapter/Client1.java
+++ src/org/sd/Adapter/Client1.java
@@ -1,16 +1,18 @@
 package org.sd.Adapter;
 
-public class Client1 {
-    public void test1(Target target)
-    {
-        target.handleReq();
-    }
+public class Client1 extends Person{
 
     public static void main(String[] args) {
-        Client1 c=new Client1();
-        Adaptee adaptee=new Adaptee();
+        Person p=new Client1();
+        System.out.println(p.getName());
 
-        Target t=new Adapter(adaptee);
-        c.test1(t);
+    }
+}
+class Person{
+    private String name="person";
+    int age=0;
+
+    public String getName() {
+        return name;
     }
 }
