6ca5be92176c97f7e2d1b92facbaf4eb8fcda91a
ZBL <813321674@qq.com>
2019-09-04 13:02:37 +0800

    2019.09.04

--- src/Gof/Singleton/SingletonTest.java
+++ src/Gof/Singleton/SingletonTest.java
@@ -2,13 +2,18 @@ package Gof.Singleton;
 
 public class SingletonTest {
     public static void main(String[] args) {
+
         SingleTon2 instance1= SingleTon2.getInstance();
         SingleTon2 instance2=SingleTon2.getInstance();
         System.out.println(instance1.hashCode());
         System.out.println(instance2.hashCode());
+        Singleton8 s1=Singleton8.INSTACNE;
+        Singleton8 s2=Singleton8.INSTACNE;
+        System.out.println(s1==s2);
     }
 }
 
+//饿汉式（静态常量）
 class SingleTon1{
 
     private SingleTon1()
@@ -23,7 +28,7 @@ class SingleTon1{
         return instance;
     }
 }
-
+//饿汉式（静态代码块）
 class SingleTon2{
 
     private SingleTon2(){}
@@ -36,5 +41,91 @@ class SingleTon2{
     public static SingleTon2 getInstance()
     {
         return instance;
+    }
+}
+
+//懒汉式（线程不安全）
+class Singleton3
+{
+    private static Singleton3 instance;
+    private Singleton3(){}
+    public static Singleton3 getInstance()
+    {
+        if(instance==null)
+        {
+            instance=new Singleton3();
+        }
+        return instance;
+    }
+}
+
+//懒汉式（线程安全，同步方法）
+class Singleton4
+{
+    private static Singleton4 instance;
+    private Singleton4(){}
+    public static synchronized Singleton4 getInstance()
+    {
+        if(instance==null)
+            instance=new Singleton4();
+        return instance;
+    }
+
+}
+
+//懒汉式（同步代码块，不推荐）
+class Singleton5
+{
+    private static Singleton5 instance;
+    private Singleton5(){}
+    public static Singleton5 getInstance()
+    {
+        if(instance==null)
+            synchronized (Singleton5.class) {
+                instance = new Singleton5();
+            }
+        return instance;
+    }
+}
+
+//双重检查，同步代码块，线程安全
+class Singleton6
+{
+    private static volatile Singleton6 instance;
+    private Singleton6(){}
+    //提供了一个静态的共有方法，加入了双重检查，解决了线程安全问题，同时解决了懒加载问题，保证了效率，推荐
+    public static Singleton6 getInstance(){
+        if(instance==null)
+        {
+            synchronized (Singleton6.class)
+            {
+                if(instance==null)
+                    instance=new Singleton6();
+            }
+        }
+        return instance;
+    }
+}
+//静态内部类
+class Singleton7
+{
+     private static volatile Singleton7 instance;
+private Singleton7(){}
+     private static class SingletonInstance{
+    private static final Singleton7 INSTANCE =new Singleton7();
+}
+
+    public static Singleton7 getInstance()
+    {
+        return SingletonInstance.INSTANCE;
+    }
+}
+
+//枚举
+enum Singleton8{
+    INSTACNE;//属性
+    public void sayOK()
+    {
+        System.out.println("ok");
     }
 }
