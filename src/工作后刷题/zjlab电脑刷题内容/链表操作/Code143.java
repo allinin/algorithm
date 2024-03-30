73cd07603f82480d9ac695c45536ec462dc15ee3
allinin <813321674@qq.com>
2024-03-30 15:12:13 +0800

    链表操作25,143

---
+++ src/工作后刷题/zjlab电脑刷题内容/链表操作/Code143.java
@@ -0,0 +1,74 @@
+package 工作后刷题.zjlab电脑刷题内容.链表操作;
+
+import 工作后刷题.zjlab电脑刷题内容.HOT100和TOP面试题.ListNode;
+
+/**
+ * @Author:zbl
+ * @Date:2024/3/30 14:43
+ * 重排链表(medium)
+ * 给定一个单链表 L 的头节点 head ，单链表 L 表示为：
+ * <p>
+ * L0 → L1 → … → Ln - 1 → Ln
+ * 请将其重新排列后变为：
+ * <p>
+ * L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+ * 不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4]
+ * 输出：[1,4,2,3]
+ * 示例 2：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[1,5,2,4,3]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 链表的长度范围为 [1, 5 * 104]
+ * 1 <= node.val <= 1000
+ */
+public class Code143 {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode newHead = slow.next;
+        //断开
+        slow.next = null;
+        newHead = reverse(newHead);
+        slow = head;
+        while (slow != null && newHead != null) {
+            ListNode slowNext = slow.next;
+            ListNode newNext = newHead.next;
+            slow.next = newHead;
+            newHead.next = slowNext;
+            slow = slowNext;
+            newHead = newNext;
+        }
+
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null, cur = head, next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
---
+++ src/工作后刷题/zjlab电脑刷题内容/链表操作/Code25.java
@@ -0,0 +1,90 @@
+package 工作后刷题.zjlab电脑刷题内容.链表操作;
+
+import 工作后刷题.zjlab电脑刷题内容.HOT100和TOP面试题.*;
+
+/**
+ * @Author:zbl
+ * @Date:2024/3/30 14:45
+ * k个一组翻转链表(hard)
+ * 给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
+ * <p>
+ * k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * <p>
+ * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5], k = 2
+ * 输出：[2,1,4,3,5]
+ * 示例 2：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5], k = 3
+ * 输出：[3,2,1,4,5]
+ * <p>
+ * <p>
+ * 提示：
+ * 链表中的节点数目为 n
+ * 1 <= k <= n <= 5000
+ * 0 <= Node.val <= 1000
+ * <p>
+ * <p>
+ * 进阶：你可以设计一个只用 O(1) 额外内存空间的算法解决此问题吗？
+ */
+public class Code25 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        int sum = 0;
+        ListNode cur = head;
+        while (cur != null) {
+            cur = cur.next;
+            sum++;
+        }
+        if (sum < k) {
+            return head;
+        }
+        int reverseTime = sum / k;
+        ListNode start = head, nowEnd = null, ans = null;
+        for (int i = 0; i < reverseTime; i++) {
+            ListNode[] res = reverse(start, k);
+            start = res[2];
+
+            if (ans == null) {
+                ans = res[1];
+            }
+
+            if (nowEnd == null) {
+                nowEnd = res[0];
+            } else {
+                nowEnd.next = res[1];
+                nowEnd = res[0];
+            }
+
+        }
+        //连接起来
+        nowEnd.next = start;
+        return ans;
+    }
+
+    private ListNode[] reverse(ListNode head, int k) {
+        ListNode[] res = new ListNode[3];
+        res[0] = head; //从head位置开始翻转k个后子链表的末尾节点
+        ListNode cur = head, pre = null, next = null;
+        while (k > 0) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+            --k;
+        }
+        res[1] = pre;//从head位置开始翻转k个后子链表的起始节点
+        res[2] = cur;//下一次翻转的起始节点
+        return res;
+    }
+}
